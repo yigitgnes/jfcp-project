009be0f056e7b76617caa9d41420204282220360
Yigit Mustafa Gunes <yigit.gnes@gmail.com>
2022-07-16 22:20:42 +0200

    missing parts

---
+++ portfolio-starter/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
